a59041621269cab1b2a2615c1e401aa0351c2515
dylanmeh <89469261+dylanmeh@users.noreply.github.com>
2021-11-10 18:24:18 -0500

    Create buildResults.groovy

---
+++ vars/buildResults.groovy
@@ -0,0 +1,20 @@
+def call()
+post {
+      success {
+        emailext (
+        subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """SUCCESSFUL: Job '${JOB_NAME} [${BUILD_NUMBER}]':
+        Check console output at ${BUILD_URL}""",
+        to: 'dylan.mehmedovic@concanon.com'
+        )
+      }
+      failure {
+        emailext (
+        subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """FAULURE: Job '${JOB_NAME} [${BUILD_NUMBER}]':
+        Check console output at ${BUILD_URL}""",
+        to: 'dylan.mehmedovic@concanon.com'
+      )
+    }   
+  }
+}
