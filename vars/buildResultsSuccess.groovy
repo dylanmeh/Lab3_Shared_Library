d27f0e7558732d2f888ffef88cc96426dab11ff9
dylanmeh <89469261+dylanmeh@users.noreply.github.com>
2021-11-10 21:14:25 -0500

    Update buildResultsSuccess.groovy

--- vars/buildResultsSuccess.groovy
+++ vars/buildResultsSuccess.groovy
@@ -1,7 +1,7 @@
-def call() {
+def call(String success) {
   emailext (
-  subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
-  body: """SUCCESSFUL: Job '${JOB_NAME} [${BUILD_NUMBER}]':
+  subject: "${success}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+  body: """${success}: Job '${JOB_NAME} [${BUILD_NUMBER}]':
   Check console output at ${BUILD_URL}""",
   to: 'dylan.mehmedovic@concanon.com'
   )
