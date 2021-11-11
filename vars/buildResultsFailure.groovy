162a1f4b252e3998c5f95092bbe69750612309a0
dylanmeh <89469261+dylanmeh@users.noreply.github.com>
2021-11-10 19:38:46 -0500

    Create buildResultsFailure.groovy

---
+++ vars/buildResultsFailure.groovy
@@ -0,0 +1,7 @@
+def call() {
+        emailext (
+        subject: "FAILURE: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+        body: """FAULURE: Job '${JOB_NAME} [${BUILD_NUMBER}]':
+        Check console output at ${BUILD_URL}""",
+        to: 'bilal.hussain@concanon.com'
+        )
